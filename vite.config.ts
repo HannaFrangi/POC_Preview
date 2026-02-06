cb5d6d0001874d0f4b2737316cdce12dd638b47b
HannaFrangi <HannaFrangi.hf@gmail.com>
2026-02-06 17:20:24 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/POC_Preview/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
